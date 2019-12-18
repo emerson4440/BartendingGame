7858562818ada40aff0f6a47c65e775f1f250b94
Matthijs <525764@student.glu.nl>
2019-12-18 13:01:40 +0100

    UI buttons working

---
+++ Bartendergame/Assets/Matthijs/Scripts/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Panel currentPanel = null;
+
+    private List<Panel> panelHistory = new List<Panel>();
+
+    private void Start()
+    {
+        SetupPanels();
+    }
+
+    private void SetupPanels()
+    {
+        Panel[] panels = GetComponentsInChildren<Panel>();
+
+        foreach (Panel panel in panels)
+            panel.Setup(this);
+
+        currentPanel.Show();    
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void GoToPrevious()
+    {
+
+    }
+
+    public void SetCurrentWithHistory(Panel newPanel)
+    {
+        panelHistory.Add(currentPanel);
+        SetCurrent(newPanel);
+    }
+
+    private void SetCurrent(Panel newPanel)
+    {
+        currentPanel.Hide();
+
+        currentPanel = newPanel;
+        currentPanel.Show();
+    }
+}
