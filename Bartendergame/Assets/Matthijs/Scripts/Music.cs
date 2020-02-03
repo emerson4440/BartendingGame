7cc0487a61d77b2e39531c3339098347fd4f7e68
Matthijs <525764@student.glu.nl>
2020-02-03 11:26:35 +0100

    meer shit die af is

---
+++ Bartendergame/Assets/Matthijs/Scripts/Music.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
+    }
+}
