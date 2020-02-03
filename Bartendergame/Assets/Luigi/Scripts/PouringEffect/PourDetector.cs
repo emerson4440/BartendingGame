0ced65f99b1e18c56e0b8bd3066cae4c2eee7c5b
LuigiOvertoom <firedream2236@gmail.com>
2020-02-03 14:04:10 +0100

    Pouring Added

---
+++ Bartendergame/Assets/Luigi/Scripts/PouringEffect/PourDetector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int m_PourThreshold = 45;
+    public Transform m_Origin = null;
+    public GameObject m_StreamPrefab = null;
+
+    private bool m_IsPouring = false;
+    private Stream m_CurrentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < m_PourThreshold;
+
+        if (m_IsPouring != pourCheck)
+        {
+            m_IsPouring = pourCheck;
+
+            if (m_IsPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        print("Start");
+        m_CurrentStream = CreateStream();
+        m_CurrentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("End");
+        m_CurrentStream.End();
+        m_CurrentStream = null;
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(m_StreamPrefab, m_Origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+}
---
+++ Bartendergame/Assets/Luigi/Scripts/PouringEffect/Stream.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour
+{
+    private LineRenderer m_LineRenderer = null;
+    private ParticleSystem m_SplashParticle = null;
+
+    private Coroutine m_PourRoutine = null;
+    private Vector3 m_targetPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        m_LineRenderer = GetComponent<LineRenderer>();
+        m_SplashParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(UpdateParticle());
+        m_PourRoutine = StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+        while (gameObject.activeSelf)
+        {
+            m_targetPosition = FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            MoveToPosition(1, m_targetPosition);
+
+            yield return null;
+        }        
+    }
+
+    public void End()
+    {
+        StopCoroutine(m_PourRoutine);
+        m_PourRoutine = StartCoroutine(EndPour());
+    }
+
+    private IEnumerator EndPour()
+    {
+        while (!HasReachedPosition(0, m_targetPosition))
+        {
+            AnimateToPosition(0, m_targetPosition);
+            AnimateToPosition(1, m_targetPosition);
+
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 2.0f);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(2.0f);
+
+        return endPoint;
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        m_LineRenderer.SetPosition(index, targetPosition);
+    }
+
+    private void AnimateToPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPoint = m_LineRenderer.GetPosition(index);
+        Vector3 newPosition = Vector3.MoveTowards(currentPoint, targetPosition, Time.deltaTime * 1.75f);
+        m_LineRenderer.SetPosition(index, newPosition);
+    }
+
+    private bool HasReachedPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPosition = m_LineRenderer.GetPosition(index);
+        return currentPosition == targetPosition;
+    }
+
+    private IEnumerator UpdateParticle()
+    {
+        while (gameObject.activeSelf)
+        {
+            m_SplashParticle.gameObject.transform.position = m_targetPosition;
+
+            bool isHitting = HasReachedPosition(1, m_targetPosition);
+            m_SplashParticle.gameObject.SetActive(isHitting);
+
+            yield return null;
+        }
+    }
+}
